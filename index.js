29b5b1e0b4ece75e396c2943f95be59ad4f3e9b4
Shamayil Ahmad <shamayil.amd@gmail.com>
2025-07-07 17:18:25 +0530

    Added Delete API

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const initializeDatabase = require("./db/db.connect.js");
 const express = require("express");
 const Job = require("./models/job.model.js")
 const jobs = require("./jobs.json")
-    // const cors = require("cors");
+const cors = require("cors");
+corsOption = {
+    origin: "*",
+    credentials: true
+}
+
+
 initializeDatabase();
 const app = express();
 app.use(express.json());
+app.use(cors(corsOption))
+
 
 app.post("/jobs", async(req, res) => {
     try {
@@ -38,6 +46,20 @@ app.get("/jobs", async(req, res) => {
     }
 })
 
+app.delete("/jobs/:id", async(req, res) => {
+    try {
+        const jobDeleted = await Job.findByIdAndDelete(req.params.id);
+        if (jobDeleted) {
+            res.status(200).json({ message: "Deleted", job: jobDeleted })
+        } else {
+            res.status(404).json({ message: "Job Not Found" })
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 const addData = async(jobs) => {
 
     try {
@@ -55,6 +77,8 @@ const addData = async(jobs) => {
     }
 
 }
+
+// addData(jobs)
 
 const PORT = 3000;
 
